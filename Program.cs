73bbc50bccae94dfac11acdb5dbc5e6b31158497
fad16papa <fad16papa@gmail.com>
2022-11-12 18:53:27 +0800

    Commit changes

--- Program.cs
+++ Program.cs
@@ -1,30 +1,23 @@
 ﻿using System;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
-using WhatsAppBot.Models;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Find your Account SID and Auth Token at twilio.cosm/console
+        // Find your Account SID and Auth Token at twilio.com/console
         // and set the environment variables. See http://twil.io/secure
-
-        // var test = new AuthenticateModel()
-        // {
-        //     TwilioAccountSID = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID"),
-        //     TwilioAuthToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN")
-        // };
-
         string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
-        string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+        string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN,");
 
-        TwilioClient.Init("ACa38908aeb81739ea6babd3fb0fa28a58", "9e78fa59e26288cdbe4ab6b4bbdd7a5e");
+        TwilioClient.Init(accountSid, authToken);
 
         var message = MessageResource.Create(
-            body: "Hello there test fad!",
-            from: new Twilio.Types.PhoneNumber("whatsapp:+14155238886"),
-            to: new Twilio.Types.PhoneNumber("whatsapp:+6593395840")
+            body: "Hello there!",
+            from: new Twilio.Types.PhoneNumber("whatsapp:+6593395840"),
+            to: new Twilio.Types.PhoneNumber("whatsapp:+15005550006")
         );
 
         Console.WriteLine(message.Sid);
